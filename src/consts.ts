1c8f7aeabe29198d675837e1ea644c37ac4b290c
jpasholk <josh.pasholk+github@gmail.com>
2023-11-25 09:43:52 -0800

    Testing...

--- src/consts.ts
+++ src/consts.ts
@@ -106,6 +106,6 @@ export const NAVIGATION = [
     {href: '/', title: 'Home'},
     {href: '/blog', title: 'Blog'},
     {href: '/tags', title: 'Tags'},
-    // {href: '/projects', title: 'Projects'},
+    {href: '/projects', title: 'Projects'},
     {href: '/about', title: 'About'},
 ]
